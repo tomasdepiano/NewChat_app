6d340196955c84e5cfa60bebae1b4055a0f08e89
Tomas De Piano <tomasdepiano1@gmail.com>
2024-04-06 18:33:36 -0600

    Created the first api request and added dummy data to show data on the site

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 import express from "express";
+import { chats } from "./data/data.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(4000, console.log("Server started on PORT 4000"));
+app.get("/", (req, res) => {
+  res.send("API is running successfully!");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
