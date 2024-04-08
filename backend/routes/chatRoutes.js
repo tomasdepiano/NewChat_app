f272b9401906c43a65138711d7736349d3dc5515
Tomas De Piano <tomasdepiano1@gmail.com>
2024-04-08 11:47:11 -0600

    Created all the endpoints for group message, rename group message, add and remove a user from a group chat; tested it all in postman and it is working perfectly

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,19 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import accessChat from "../controllers/chatControllers.js";
+import { accessChat } from "../controllers/chatControllers.js";
+import { fetchChats } from "../controllers/chatControllers.js";
+import { createGroupChat } from "../controllers/chatControllers.js";
+import { renameGroup } from "../controllers/chatControllers.js";
+import { addToGroup } from "../controllers/chatControllers.js";
+import { removeFromGroup } from "../controllers/chatControllers.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChat);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 export default router;
