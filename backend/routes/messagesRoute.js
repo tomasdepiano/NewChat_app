4052c5c044f74e23a5bc2dfe6297e63e6b5d74b5
Tomas De Piano <tomasdepiano1@gmail.com>
2024-04-11 07:08:00 -0600

    Got rid of an annoying bug that wouldn't even show up the app in dev mode; the chatprovider wasn't being wrapped by the browserRoute in index.js; tested it out on the site and everything works fine; started to write the code for one to one messages but still need to work on it

---
+++ backend/routes/messagesRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
+import sendMessage from "../controllers/messageControllers.js";
+// import { allMessages } from "../controllers/messageControllers.js";
+
+const router = express.Router();
+
+router.route("/").post(protect, sendMessage);
+// router.route("/:chatId").get(protect, allMessages);
+
+export default router;
--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -1,21 +1,21 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  const [user, setUser] = useState();
   const [selectedChat, setSelectedChat] = useState();
-  const [chats, setChats] = useState([]);
-  const history = useHistory;
+  const [user, setUser] = useState();
+  // const [notification, setNotification] = useState([]);
+  const [chats, setChats] = useState();
+
+  const history = useHistory();
 
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     setUser(userInfo);
 
-    if (!userInfo) {
-      history.pushState("/");
-    }
+    if (!userInfo) history.push("/");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [history]);
 
   return (
